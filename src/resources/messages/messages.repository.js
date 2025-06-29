515e78ebfb30bac059c69efbf4e1aa634f6bd0e0
Shaik Suhail <suhailshaik975@gmail.com>
2025-06-29 20:24:56 +0530

    Fixed the messages sorting and updated the old Dockerfile

--- src/resources/messages/messages.repository.js
+++ src/resources/messages/messages.repository.js
@@ -2,123 +2,121 @@ import mongoose from "mongoose";
 import Message from "./messages.model.js";
 
 export default class MessagesRepository {
-
   static async getMessages(user) {
-  const userId = new mongoose.Types.ObjectId(user);
-  const messages = await Message.aggregate([
-    {
-      $match: {
-        $or: [{ sender: userId }, { receiver: userId }],
+    const userId = new mongoose.Types.ObjectId(user);
+    const messages = await Message.aggregate([
+      {
+        $match: {
+          $or: [{ sender: userId }, { receiver: userId }],
+        },
       },
-    },
-    {
-      $addFields: {
-        createdAtDate: { $toDate: "$createdAt" }
-      }
-    },
-    {
-      $addFields: {
-        threadId: {
-          $cond: {
-            if: { $gt: ["$sender", "$receiver"] },
-            then: {
-              $concat: [
-                { $toString: "$receiver" },
-                "_",
-                { $toString: "$sender" },
-              ],
-            },
-            else: {
-              $concat: [
-                { $toString: "$sender" },
-                "_",
-                { $toString: "$receiver" },
-              ],
+      {
+        $addFields: {
+          createdAtDate: { $toDate: "$createdAt" },
+        },
+      },
+      {
+        $addFields: {
+          threadId: {
+            $cond: {
+              if: { $gt: ["$sender", "$receiver"] },
+              then: {
+                $concat: [
+                  { $toString: "$receiver" },
+                  "_",
+                  { $toString: "$sender" },
+                ],
+              },
+              else: {
+                $concat: [
+                  { $toString: "$sender" },
+                  "_",
+                  { $toString: "$receiver" },
+                ],
+              },
             },
           },
         },
       },
-    },
-    // Sort by the reliable createdAtDate to ensure we get the truly last message
-    { $sort: { createdAtDate: -1 } },
-    {
-      $group: {
-        _id: "$threadId",
-        message: { $first: "$message" },
-        sender: { $first: "$sender" },
-        receiver: { $first: "$receiver" }, 
-        originalMessageSentAt: { $first: "$messageSentAt" },
-        latestCreatedAt: { $first: "$createdAtDate" },
+      // Sort by the reliable createdAtDate to ensure we get the truly last message
+      { $sort: { createdAtDate: -1 } },
+      {
+        $group: {
+          _id: "$threadId",
+          message: { $first: "$message" },
+          sender: { $first: "$sender" },
+          receiver: { $first: "$receiver" },
+          originalMessageSentAt: { $first: "$messageSentAt" },
+          latestCreatedAt: { $first: "$createdAtDate" },
+        },
       },
-    },
-    {
-      $addFields: {
-        otherUserId: {
-          $cond: {
-            if: { $eq: ["$sender", userId] },
-            then: "$receiver",
-            else: "$sender",
+      {
+        $addFields: {
+          otherUserId: {
+            $cond: {
+              if: { $eq: ["$sender", userId] },
+              then: "$receiver",
+              else: "$sender",
+            },
           },
         },
       },
-    },
-    {
-      $lookup: {
-        from: "users",
-        localField: "otherUserId",
-        foreignField: "_id",
-        as: "otherUser",
+      {
+        $lookup: {
+          from: "users",
+          localField: "otherUserId",
+          foreignField: "_id",
+          as: "otherUser",
+        },
       },
-    },
-    {
-      $unwind: "$otherUser",
-    },
-    {
-      $project: {
-        _id: 0,
-        lastMessage: "$message",
-        messageSentAt: "$originalMessageSentAt",
-        otherUserId: 1,
-        username: "$otherUser.username",
-        profilePicture: "$otherUser.profilePicture",
-        latestCreatedAt: 1,
+      {
+        $unwind: "$otherUser",
       },
-    },
-    // Final sort based on the reliable latestCreatedAt timestamp for conversations order
-    { $sort: { latestCreatedAt: -1 } },
-  ]);
-  return messages;
-}
+      {
+        $project: {
+          _id: 0,
+          lastMessage: "$message",
+          messageSentAt: "$originalMessageSentAt",
+          otherUserId: 1,
+          username: "$otherUser.username",
+          profilePicture: "$otherUser.profilePicture",
+          latestCreatedAt: 1,
+        },
+      },
+      // Final sort based on the reliable latestCreatedAt timestamp for conversations order
+      { $sort: { latestCreatedAt: -1 } },
+    ]);
+    return messages;
+  }
 
   static async getUnreadMessages(user) {
-  const userId = new mongoose.Types.ObjectId(user);
+    const userId = new mongoose.Types.ObjectId(user);
 
-  return await Message.aggregate([
-    {
-      $match: {
-        receiver: userId,
-        messageRead: false,
+    return await Message.aggregate([
+      {
+        $match: {
+          receiver: userId,
+          messageRead: false,
+        },
       },
-    },
-    {
-      $sort: {
-        createdAt: 1,
+      {
+        $sort: {
+          createdAt: 1,
+        },
       },
-    },
-    {
-      $project: {
-        _id: 1,
-        sender: 1,
-        receiver: 1,
-        message: 1,
-        messageRead: 1,
-        messageSentAt: 1,
-        fullTime: 1,
+      {
+        $project: {
+          _id: 1,
+          sender: 1,
+          receiver: 1,
+          message: 1,
+          messageRead: 1,
+          messageSentAt: 1,
+          fullTime: 1,
+        },
       },
-    },
-  ]);
-}
-
+    ]);
+  }
 
   static async getUserMessages(senderId, receiverId) {
     const sender = new mongoose.Types.ObjectId(senderId);
@@ -139,6 +137,7 @@ export default class MessagesRepository {
           message: 1,
           fullTime: 1,
           messageSentAt: 1,
+          createdAt: 1,
           user: {
             $eq: ["$sender", sender],
           },
@@ -146,10 +145,9 @@ export default class MessagesRepository {
       },
       {
         $sort: {
-          fullTime: 1,
+          createdAt: 1,
         },
       },
     ]);
   }
-
 }
